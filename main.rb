9420a88d962a3843c1b367a96f01fba3d6d0933e
tact <tactppp@gmail.com>
2016-01-17 17:28:44 +0900

    ページにタイムラインを表示させるようにしました。

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'twitter'
+require 'dotenv'
+
+before do
+  Dotenv.load
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = 'SkYGENE0aGE8ubLMSk48EK8Q0'
+    config.consumer_secret     = ENV['CONSUMER_SECRET']
+    config.access_token        = '140300834-qZM9uKvSSMg8cVvME82M0UcclATMcbOATKX2iZuW'
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  end
+  @items = client.home_timeline
+end
+
+get '/' do
+  erb :sinatra_rensyu
+end
